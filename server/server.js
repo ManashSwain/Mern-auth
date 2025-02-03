6dd582f08557e6011543658046383503b80fa8f3
ManashSwain <manash0802@gmail.com>
2025-02-03 13:02:04 +0530

    implemented db connection

--- server/server.js
+++ server/server.js
@@ -1,4 +1,7 @@
 import express from "express";
+import connectDB from "./utils/connectDB.js";
+import dotenv from "dotenv" ;
+dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 4000;
@@ -9,6 +12,9 @@ app.use(express.json());
 app.get("/" , (req,res)=>{
    res.status(200).send("Hello from server!");
 })
+
+// database connection function 
+connectDB();
 
 app.listen(port , ()=>{
     console.log(`Server is listening on port ${port}`);
