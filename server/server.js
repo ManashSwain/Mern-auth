62af31804080479a5bcdd2d2c5606e0c6cb4a89a
ManashSwain <manash0802@gmail.com>
2025-02-03 13:45:52 +0530

    added connectdb function in the right place

--- server/server.js
+++ server/server.js
@@ -1,20 +1,28 @@
 import express from "express";
 import connectDB from "./utils/connectDB.js";
 import dotenv from "dotenv" ;
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+// dotenv configuration
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 4000;
 
+
+// database connection function 
+connectDB();
+
 // middlewares 
 app.use(express.json());
+app.use(cors({credentials : true}));
+app.use(cookieParser());
 
 app.get("/" , (req,res)=>{
    res.status(200).send("Hello from server!");
 })
 
-// database connection function 
-connectDB();
 
 app.listen(port , ()=>{
     console.log(`Server is listening on port ${port}`);
